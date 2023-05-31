b1946e0fd49524c9847164e8d041ad6fb4009927
mbc2579 <mbc2579@naver.com>
2023-06-01 08:48:01 +0900

    sparta3(4)

--- src/homeworks/kiosk2/Main.java
+++ src/homeworks/kiosk2/Main.java
@@ -13,6 +13,8 @@ public class Main {
         Ade ades = new Ade();
         Etc etcs = new Etc();
         Order order = new Order();
+        int count = 0;
+        double total = 0;
 
 
 
@@ -36,6 +38,7 @@ public class Main {
                         order.allList.add("크뽕 | W 9.9  | 베스트 메뉴!! 크림소스를 사용한 크림파스타!!");
                         System.out.println("크뽕이 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 9.9;
                     }
                 } else if(menus2.equals("2")) {
                     ppongs.lppong();
@@ -44,6 +47,7 @@ public class Main {
                         order.allList.add("로뽕 | W 10.9 | 로제소스&모짜렐라 치즈로 풍미를 한번 더!!");
                         System.out.println("로뽕이 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 10.9;
                     }
                 } else if(menus2.equals("3")) {
                     ppongs.cppong();
@@ -52,6 +56,7 @@ public class Main {
                         order.allList.add("차뽕 | W 8.5  | 1, 2, 3단계의 매콤한 국물!! 원하는 단계를 선택해 내 취향에 맞게!!");
                         System.out.println("차뽕이 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 8.5;
                     }
                 }
             } else if (menus1.equals("2")) {
@@ -64,6 +69,7 @@ public class Main {
                         order.allList.add("니피자 | W 10.9  | 달콤고소한 고구마피자, 매콤한 짬뽕과 최고의 조합");
                         System.out.println("니피자가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 10.9;
                     }
                 } else if(menus2.equals("2")) {
                     pizzas.mypizza();
@@ -72,6 +78,7 @@ public class Main {
                         order.allList.add("내피자 | W 10.9  | 감칠맛 나는 갈릭버터로 크림치즈 딥소스에 찍어 먹는 스테디셀러");
                         System.out.println("내피자가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 10.9;
                     }
                 } else if(menus2.equals("3")) {
                     pizzas.pepizza();
@@ -80,6 +87,7 @@ public class Main {
                         order.allList.add("페피자 | W 12.9  | 트리플치즈의 풍미와 페퍼로니의 만남");
                         System.out.println("페피자가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 12.9;
                     }
                 }
             } else if (menus1.equals("3")) {
@@ -92,6 +100,7 @@ public class Main {
                         order.allList.add("청포도에이드(500ML) | W 4.0  | 청포도에이드");
                         System.out.println("청포도에이드(500ML)가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 4.0;
                     }
                 } else if(menus2.equals("2")) {
                     ades.applemango();
@@ -100,6 +109,7 @@ public class Main {
                         order.allList.add("애플망고에이드(500ML) | W 4.0  | 애플망고에이드");
                         System.out.println("애플망고에이드(500ML)가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 4.0;
                     }
                 } else if(menus2.equals("3")) {
                     ades.Orange();
@@ -108,6 +118,7 @@ public class Main {
                         order.allList.add("오렌지에이드(500ML) | W 4.0  | 오렌지에이드");
                         System.out.println("오렌지에이드(500ML)가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 4.0;
                     }
                 }
             } else if (menus1.equals("4")) {
@@ -120,6 +131,7 @@ public class Main {
                         order.allList.add("면사리 | W 1.0  | 면사리 추가");
                         System.out.println("면사리가 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 1.0;
                     }
                 } else if(menus2.equals("2")) {
                     etcs.rice();
@@ -128,6 +140,7 @@ public class Main {
                         order.allList.add("공기밥 | W 1.0  | 공기밥 추가");
                         System.out.println("공기밥이 장바구니에 추가되었습니다.");
                         System.out.println();
+                        total += 1.0;
                     }
                 }
             } else if(menus1.equals("5")) {
@@ -135,8 +148,37 @@ public class Main {
                 order.fors();
                 System.out.println();
                 System.out.println("[ Total ]");
-
+                // 토탈
+                System.out.printf("W %.1f\n", total);
                 System.out.println();
+                System.out.println("1. 주문        2. 메뉴판");
+                String menus2 = sc.nextLine();
+                if(menus2.equals("1")) {
+                    System.out.println("주문이 완료되었습니다!");
+                    count += 1;
+                    System.out.println();
+                    System.out.println("대기번호는 [ " + count + " ] 번 입니다.");
+                    System.out.println("(3초후 메뉴판으로 돌아갑니다.)");
+                    order.allList.clear();
+                    total = 0;
+                    Thread thread = new Thread();
+                    try {
+                        thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    thread.interrupt();
+                    System.out.println();
+                }
+            } else if(menus1.equals("6")) {
+                order.cart2();
+                String menus2 = sc.nextLine();
+                if(menus2.equals("1")) {
+                    System.out.println("진행하던 주문이 취소되었습니다.");
+                    System.out.println();
+                    order.allList.clear();
+                    total = 0;
+                }
             }
         }
 
--- src/homeworks/kiosk2/Order.java
+++ src/homeworks/kiosk2/Order.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Order {
 
+    Thread thread = new Thread();
+
     ArrayList<String> allList = new ArrayList<>();
 
     public void fors() {
@@ -17,4 +19,11 @@ public class Order {
         System.out.println();
         System.out.println("[ Orders ]");
     }
+
+    // 주문 취소 메서드
+    public void cart2() {
+        System.out.println("진행하던 주문을 취소하시겠습니까?");
+        System.out.println("1. 확인        2. 취소");
+    }
+    
 }
